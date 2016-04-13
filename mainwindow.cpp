1630878afbf2c3af8b49d55e94bfc7a47e580349
MshGit2016 <1987582077@qq.com>
2016-04-13 21:59:56 +0800

    版本v1.0

---
+++ mainwindow.cpp
@@ -0,0 +1,35 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QDebug>
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    ui->comboBox->setGeometry(50,50,200,30);
+
+    ui->comboBox->addItem(QIcon(":/Resources/clock"),"Application");
+    ui->comboBox->addItem(QIcon(":/Resources/save"),"Graphics");
+    ui->comboBox->addItem(QIcon(":/Resources/refresh"),"Database");
+    ui->comboBox->addItem(QIcon(":/Resources/network"),"Network");
+
+    connect(ui->comboBox,SIGNAL(currentIndexChanged(int)),this,SLOT(valueChanged()));
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+void MainWindow::valueChanged()
+{
+    int current_index=ui->comboBox->currentIndex();
+    qDebug("Current ComboBox index:%d",current_index);
+
+    QString str;
+    str=ui->comboBox->currentText();
+    qDebug()<<"Current ComboBox text"<<str;
+    qDebug()<<"Total Item:"<<ui->comboBox->count();
+    ui->comboBox->removeItem(3);
+
+}
